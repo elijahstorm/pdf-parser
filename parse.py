e9d62106b73679652269f29a4619eb2914acbf4c
elijahstorm <95153181+elijahstorm@users.noreply.github.com>
2023-08-25 00:26:19 +0900

    add image generation

--- parse.py
+++ parse.py
@@ -2,14 +2,18 @@ import sys
 import openai
 import fitz
 from dotenv import load_dotenv
+from wand.image import Image
+from wand.color import Color
 import os
 
 load_dotenv()
+
 
 def read_prompt_from_file(prompt_file):
     with open(prompt_file, "r") as f:
         prompt = f.read()
     return prompt
+
 
 def generate_response_from_pdf(prompt, pdf_text, max_tokens):
     conversation = [
@@ -17,11 +21,17 @@ def generate_response_from_pdf(prompt, pdf_text, max_tokens):
         {"role": "user", "content": pdf_text},
     ]
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=conversation,
-        max_tokens=max_tokens
+        model="gpt-3.5-turbo", messages=conversation, max_tokens=max_tokens
     )
     return response.choices[0].message["content"].strip()
+
+
+def pdf_to_image(pdf_path, image_path, resolution=300, background_color="white"):
+    with Image(filename=pdf_path, resolution=resolution) as img:
+        img.background_color = Color(background_color)
+        img.alpha_channel = "remove"  # Remove alpha channel if present
+        img.save(filename=image_path)
+
 
 def extract_pdf_text(pdf_path):
     pdf_document = fitz.open(pdf_path)
@@ -31,19 +41,22 @@ def extract_pdf_text(pdf_path):
         pdf_text += page.get_text()
     return pdf_text
 
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Usage: python parse.py input_pdf_file.pdf [max_tokens] [output_csv_file]")
+        print(
+            "Usage: python parse.py input_pdf_file.pdf [max_tokens] [output_csv_file]"
+        )
         sys.exit(1)
-    
-    input_pdf_file = sys.argv[1]
-    pdf_text = extract_pdf_text(input_pdf_file)
+
     api_key = os.getenv("OPENAI_API_KEY")
     if not api_key:
         print("API key not found in .env file.")
         sys.exit(1)
-    
-    openai.api_key = api_key
+
+    input_pdf_file = sys.argv[1]
+    image_path = image_path = "image_outputs/" + input_pdf_file.replace(".pdf", ".jpg")
+    pdf_to_image(input_pdf_file, image_path)
 
     output_csv_file = (
         "outputs/" + input_pdf_file.replace(".pdf", ".csv")
@@ -56,10 +69,12 @@ if __name__ == "__main__":
 
     prompt = read_prompt_from_file("prompt.txt")
     max_tokens = int(sys.argv[2]) if len(sys.argv) >= 3 else 100
-    response = generate_response_from_pdf(prompt, pdf_text, max_tokens)
 
-    with open(output_csv_file, "w") as f:
-        # f.write('"Question Text","Question Type","Options"\n')
-        f.write(f'{response}\n')
+    # openai.api_key = api_key
+    # response = generate_response_from_pdf(prompt, pdf_text, max_tokens)
+
+    # with open(output_csv_file, "w") as f:
+    #     f.write('"Question Text","Question Type","Options"\n')
+    #     f.write(f"{response}\n")
 
     print(f"Response saved in {output_csv_file}")
